feb25e110ed27227c8f6ef0a11d94e1df5b54cac
BizAcademy <replit@buzzbooster.app>
2026-09-06 14:16:59 +0000

    Accepter directement le format Copy as JSON de Supabase pour importer les 810 anciens utilisateurs

--- dist-deploy/api-server/index.mjs
+++ dist-deploy/api-server/index.mjs
@@ -53409,7 +53409,7 @@ function getMysqlPool() {
 
 // src/routes/health.ts
 var router = (0, import_express.Router)();
-var BUILD_TIME = "2026-09-06T12:26:24.413Z";
+var BUILD_TIME = "2026-09-06T14:16:35.184Z";
 router.get("/healthz", async (_req, res) => {
   try {
     await getMysqlPool().query("SELECT 1");
